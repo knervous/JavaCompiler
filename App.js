3369c65be662e3d3b76aa17dc859f86e85fa5af0
knervous <pcj174@gmail.com>
2017-09-28 20:01:08 -0400

    Changed app

--- App.js
+++ App.js
@@ -1,32 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 import axios from 'axios'
-const client = require('socket.io-client')
-
+//import GoogleSignIn from 'react-native-google-signin'; 
+const client = require('socket.io-client') 
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      classText: 'package testapp; \r\n \r\n'
-      +' public class TestApp {  \r\n\r\n1``'
-      +' public static void main(String[] args){ \r\n \r\n }'
-      +' \r\n\r\n }'
+      classText: 'package Java; \r\n \r\n'
+      +' public class TestApp {  \r\n\r\n'
+      +' public static void main(String[] args){ \r\n '
+      +'  for(int i = 0; i < 100; i++) { \r\n'
+      +'     System.out.println(\"Testing\: " + i);' 
+      +'    \r\n }'
+      +'\r\n }'
+      +' \r\n\r\n }' 
     }
 
-    this.sendToServer = this.sendToServer.bind(this)
   }
 
-  sendToServer(e){
-    console.log('hit here')
-    var socket = client.connect('ws://localhost:15000', {reconnect: true})
+  sendToServer(a,b,c){
+    console.log('hit here 123')
+    var self = this;
+    this.socket = client.connect('ws://73.187.131.221:15000', {reconnect: true})
     
-    socket.on('connect', function (socket) {
+    this.socket.on('connect', () => {
         console.log('Connected!');
-        socket.emit('send_class_to_compile', {data: 'here is some data'})
+        this.socket.emit('send_class_to_compile', {data: self.state.classText})
     });
     
+    this.socket.on('receive_error', (data)=>{
+      Alert.alert(
+        'Java Error! Nice one idiot',
+        data,
+        [
+          //{text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+          {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: true }
+      )
+    })
+
+    this.socket.on('receive_output',(data)=>{
+      Alert.alert(
+        'Java Output From Successful Compile',
+        data,
+        [
+          //{text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+          {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: true }
+      )
+
+    })
     
+  }
+
+  async _signIn(){
+    const user = await GoogleSignIn.signInPromise();
+    console.log(user);
   }
 
   render() {
@@ -36,17 +71,21 @@ export default class App extends React.Component {
         <Text>Testing Class Compiler</Text>
         <TextInput
         multiline={true}
+        blurOnSubmit={false}
         style={{height: '70%', width: '100%', borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({classText: text})}
+        onChangeText={(text) => {
+          this.setState({classText: text})}
+          }
         value={this.state.classText}
       />
 
       <Button
-          onPress={this.sendToServer}
+          onPress={this.sendToServer.bind(this)}
           title="Send To Server"
           color="#841584"
           accessibilityLabel="Send To Server to Compile and get Response"
         />
+        
       </View>
     );
   }
